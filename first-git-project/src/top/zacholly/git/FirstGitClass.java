d83603b58e00724f3ef1d129ffbf5fb480ef4b63
zac_holly <hlz_uestc@163.com>
2018-12-04 22:26:34 +0800

    第一个git项目

---
+++ first-git-project/src/top/zacholly/git/FirstGitClass.java
@@ -0,0 +1,16 @@
+package top.zacholly.git;
+
+public class FirstGitClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
